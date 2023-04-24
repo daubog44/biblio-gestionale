e1ba342b02826073911f23e30a518ea6f9ac48b7
daubog44 <uopilul@gmail.com>
2023-04-25 00:06:01 +0200

    v0.1.8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,11 @@
 "use client";
-import {
-  checkUpdate,
-  onUpdaterEvent,
-} from '@tauri-apps/api/updater';
-import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [version, setVersion] = useState<string | undefined>("Some");
-
-  useEffect(() => {
-    let unlisten: () => void;
-    (async function () {
-      unlisten = await onUpdaterEvent(({ error, status }) => {
-        // This will log all updater events, including status updates and errors.
-        console.log('Updater event', error, status)
-      })
-
-      try {
-        const { shouldUpdate, manifest } = await checkUpdate()
-
-        if (shouldUpdate) {
-          // You could show a dialog asking the user if they want to install the update here.
-          setVersion(manifest?.version);
-          console.log(
-            `Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
-          )
-        }
-      } catch (error) {
-        console.error(error)
-      }
-    })();
-
-    return () => {
-      unlisten()
-    }
-  }, [])
 
 
   return (
-    <>
-      {version ? version : "not updates"}
-    </>
+    <main>
+      hello world
+    </main>
   )
 }
